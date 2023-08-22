05bec08929f016a98f15dbc3e890cafdaa07ec90
Chiraag_Arun_Kumar <39455997+chiraag918@users.noreply.github.com>
2023-08-22 20:59:46 +0530

    Fixed issues

--- src/components/CircularLoader.js
+++ src/components/CircularLoader.js
@@ -1,9 +1,10 @@
-import "./CircularLoader.css";
+import "./CircularLoader.scss";
 
 const CircularLoader = () => {
 	return (
 		<div class="loader-container">
-			<div class="spin"></div>
+			<div class="loader"></div>
+			<p>Please wait, setting up the simulator</p>
 		</div>
 	);
 };
